6192796704d68d67f064b18f70872816a38ef671
Jackie Chen <ecuojackie@yahoo.com>
2019-08-23 02:57:56 -0700

    Working SVG animation; work on javascript for the actual game

--- play.js
+++ play.js
@@ -1,5 +1,12 @@
 const hanger = document.querySelectorAll('#hanger path');
+const hangBody = document.querySelectorAll('#hanger-body path');
+let textArea = document.querySelector('#guess-input');
 
 for(let i = 0; i < hanger.length; i++){ 
-    console.log(`letter ${i+1} is ${hanger[i].getTotalLength()}`)
+    console.log(`letter ${i+1} is ${hanger[i].getTotalLength()}`);
 }
+
+hangBody.forEach((element, index) =>{
+    console.log(`letter ${index+1} is ${element.getTotalLength()}`);
+})
+
