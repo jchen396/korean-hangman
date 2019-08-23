f4adafbab64ad76ab380aa53a186e38e0b095740
Jackie Chen <ecuojackie@yahoo.com>
2019-08-23 06:00:09 -0700

    Added text input and error handling; work on hangul regex

--- play.js
+++ play.js
@@ -1,7 +1,10 @@
 const hanger = document.querySelectorAll('#hanger path');
 const hangBody = document.querySelectorAll('#hanger-body path');
-let textArea = document.querySelector('#guess-input');
+const textArea = document.querySelector('#guess-input');
+const hangulRegex = /[\u3131-\uD79D]/ugi;
+let errorText = document.querySelector('#error-text');
 
+// Check SVG values and display on console
 for(let i = 0; i < hanger.length; i++){ 
     console.log(`letter ${i+1} is ${hanger[i].getTotalLength()}`);
 }
@@ -10,3 +13,16 @@ hangBody.forEach((element, index) =>{
     console.log(`letter ${index+1} is ${element.getTotalLength()}`);
 })
 
+// Check for language type
+textArea.addEventListener("input", () => {
+    console.log(String(textArea).match(/[\uac00-\ud7af]|[\u1100-\u11ff]|[\u3130-\u318f]|[\ua960-\ua97f]|[\ud7b0-\ud7ff]/g));
+    if(String(textArea).match(/[\uac00-\ud7af]|[\u1100-\u11ff]|[\u3130-\u318f]|[\ua960-\ua97f]|[\ud7b0-\ud7ff]/g)){
+        errorText.innerHTML = "This is korean";
+    }else{
+        errorText.style.opacity = "1";
+        errorText.innerHTML = "*Please enter Korean characters";
+        setTimeout(() => {
+            errorText.style.opacity = "0"
+        }, 2000)
+    }
+})
