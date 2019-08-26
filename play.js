62d8661aa84f84dbbbcd6e130e5d3d2d2dfebe7f
Jackie Chen <ecuojackie@yahoo.com>
2019-08-25 19:56:27 -0700

    Implemented validation of hangul in input; proceed to hangman game js

--- play.js
+++ play.js
@@ -1,7 +1,7 @@
 const hanger = document.querySelectorAll('#hanger path');
 const hangBody = document.querySelectorAll('#hanger-body path');
-const textArea = document.querySelector('#guess-input');
 const hangulRegex = /[\u3131-\uD79D]/ugi;
+let textArea = document.querySelector('#guess-input');
 let errorText = document.querySelector('#error-text');
 
 // Check SVG values and display on console
@@ -15,9 +15,13 @@ hangBody.forEach((element, index) =>{
 
 // Check for language type
 textArea.addEventListener("input", () => {
-    console.log(String(textArea).match(/[\uac00-\ud7af]|[\u1100-\u11ff]|[\u3130-\u318f]|[\ua960-\ua97f]|[\ud7b0-\ud7ff]/g));
-    if(String(textArea).match(/[\uac00-\ud7af]|[\u1100-\u11ff]|[\u3130-\u318f]|[\ua960-\ua97f]|[\ud7b0-\ud7ff]/g)){
+    console.log(String(textArea.value).match(/[\uac00-\ud7af]|[\u1100-\u11ff]|[\u3130-\u318f]|[\ua960-\ua97f]|[\ud7b0-\ud7ff]/g));
+    if(String(textArea.value).match(/[\uac00-\ud7af]|[\u1100-\u11ff]|[\u3130-\u318f]|[\ua960-\ua97f]|[\ud7b0-\ud7ff]/g)){
+        errorText.style.opacity = "1";
         errorText.innerHTML = "This is korean";
+        setTimeout(() => {
+            errorText.style.opacity = "0"
+        }, 2000)
     }else{
         errorText.style.opacity = "1";
         errorText.innerHTML = "*Please enter Korean characters";
