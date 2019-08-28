ea74eaac8e1a93bf38a86e3caef8c6f78c569a20
Jackie Chen <ecuojackie@yahoo.com>
2019-08-28 00:41:39 -0700

    Testing function to read strings from an external text file

--- play.js
+++ play.js
@@ -1,6 +1,8 @@
+const fs = require('fs');
+
 const hanger = document.querySelectorAll('#hanger path');
 const hangBody = document.querySelectorAll('#hanger-body path');
-const hangulRegex = /[\u3131-\uD79D]/ugi;
+const hangulRegex = /[\uac00-\ud7af]|[\u1100-\u11ff]|[\u3130-\u318f]|[\ua960-\ua97f]|[\ud7b0-\ud7ff]/g;
 let textArea = document.querySelector('#guess-input');
 let errorText = document.querySelector('#error-text');
 
@@ -13,15 +15,19 @@ hangBody.forEach((element, index) =>{
     console.log(`letter ${index+1} is ${element.getTotalLength()}`);
 })
 
+// Reading and inputting korean words from a text file
+fs.readFile("./koreanWords.txt", () => {
+    if(err){
+        throw err;
+    }
+})
+
 // Check for language type
 textArea.addEventListener("input", () => {
-    console.log(String(textArea.value).match(/[\uac00-\ud7af]|[\u1100-\u11ff]|[\u3130-\u318f]|[\ua960-\ua97f]|[\ud7b0-\ud7ff]/g));
-    if(String(textArea.value).match(/[\uac00-\ud7af]|[\u1100-\u11ff]|[\u3130-\u318f]|[\ua960-\ua97f]|[\ud7b0-\ud7ff]/g)){
-        errorText.style.opacity = "1";
-        errorText.innerHTML = "This is korean";
-        setTimeout(() => {
-            errorText.style.opacity = "0"
-        }, 2000)
+    
+    console.log(textArea.value.match(hangulRegex));
+    if(textArea.value.match(hangulRegex)){
+        
     }else{
         errorText.style.opacity = "1";
         errorText.innerHTML = "*Please enter Korean characters";
