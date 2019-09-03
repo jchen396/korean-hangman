446fc0f443d9d1607dde2430c06604db8feb05d1
Jackie Chen <ecuojackie@yahoo.com>
2019-09-02 19:26:08 -0700

    Matching answer length with regex; match guess and answer

--- bundle.js
+++ bundle.js
@@ -12,12 +12,19 @@ randomWord = (hangulList[Math.floor(Math.random() * 1001)])
 module.exports = randomWord;
 },{}],2:[function(require,module,exports){
 const wordList = require('./fileRead.js');
+
+//display side variables
 const hanger = document.querySelectorAll('#hanger path');
 const hangBody = document.querySelectorAll('#hanger-body path');
+const textAnswer = document.querySelector('#text-answer');
+
+// input side variables
+const submitForm = document.querySelector("#submit-form");
 const textShow = document.querySelector('#text-show');
+const textArea = document.querySelector('#guess-input');
+const errorText = document.querySelector('#error-text');
 const hangulRegex = /[\uac00-\ud7af]|[\u1100-\u11ff]|[\u3130-\u318f]|[\ua960-\ua97f]|[\ud7b0-\ud7ff]/g;
-let textArea = document.querySelector('#guess-input');
-let errorText = document.querySelector('#error-text');
+
 
 // Check SVG values and display on console
 for(let i = 0; i < hanger.length; i++){ 
@@ -30,16 +37,24 @@ hangBody.forEach((element, index) =>{
 
 // Reading and inputting korean words from a text file
 let guessWord = wordList.split('\t').map(item => item.trim());
-englishWord = guessWord[1];
-koreanWord = guessWord[0];
+let englishWord = guessWord[1].replace(/,/g, ', ');
+let koreanWord = guessWord[0];
 textShow.innerHTML = englishWord;
+for(let i = 0; i < koreanWord.length; i++){
+    textAnswer.innerHTML += "_ ";
+}
 console.log(`${koreanWord}'s length is ${koreanWord.length}`);
+
 // Check for language type
 textArea.addEventListener("input", () => {
     
     console.log(textArea.value.match(hangulRegex));
     if(textArea.value.match(hangulRegex)){
-        
+        submitForm.addEventListener("submit", (data) => {
+            if(koreanWord === textArea.value){
+                console.log(data);
+            }
+        })
     }else{
         errorText.style.opacity = "1";
         errorText.innerHTML = "*Please enter Korean characters";
@@ -48,4 +63,6 @@ textArea.addEventListener("input", () => {
         }, 2000)
     }
 })
+
+
 },{"./fileRead.js":1}]},{},[2]);
--- play.js
+++ play.js
@@ -1,10 +1,17 @@
 const wordList = require('./fileRead.js');
+
+//display side variables
 const hanger = document.querySelectorAll('#hanger path');
 const hangBody = document.querySelectorAll('#hanger-body path');
+const textAnswer = document.querySelector('#text-answer');
+
+// input side variables
+const submitForm = document.querySelector("#submit-form");
 const textShow = document.querySelector('#text-show');
+const textArea = document.querySelector('#guess-input');
+const errorText = document.querySelector('#error-text');
 const hangulRegex = /[\uac00-\ud7af]|[\u1100-\u11ff]|[\u3130-\u318f]|[\ua960-\ua97f]|[\ud7b0-\ud7ff]/g;
-let textArea = document.querySelector('#guess-input');
-let errorText = document.querySelector('#error-text');
+
 
 // Check SVG values and display on console
 for(let i = 0; i < hanger.length; i++){ 
@@ -15,12 +22,26 @@ hangBody.forEach((element, index) =>{
     console.log(`letter ${index+1} is ${element.getTotalLength()}`);
 })
 
+// Reading and inputting korean words from a text file
+let guessWord = wordList.split('\t').map(item => item.trim());
+let englishWord = guessWord[1].replace(/,/g, ', ');
+let koreanWord = guessWord[0];
+textShow.innerHTML = englishWord;
+for(let i = 0; i < koreanWord.length; i++){
+    textAnswer.innerHTML += "_ ";
+}
+console.log(`${koreanWord}'s length is ${koreanWord.length}`);
+
 // Check for language type
 textArea.addEventListener("input", () => {
     
     console.log(textArea.value.match(hangulRegex));
     if(textArea.value.match(hangulRegex)){
-        
+        submitForm.addEventListener("submit", (data) => {
+            if(koreanWord === textArea.value){
+                console.log(data);
+            }
+        })
     }else{
         errorText.style.opacity = "1";
         errorText.innerHTML = "*Please enter Korean characters";
@@ -30,9 +51,3 @@ textArea.addEventListener("input", () => {
     }
 })
 
-// Reading and inputting korean words from a text file
-let guessWord = wordList.split('\t').map(item => item.trim());
-englishWord = guessWord[1];
-koreanWord = guessWord[0];
-textShow.innerHTML = englishWord;
-console.log(`${koreanWord}'s length is ${koreanWord.length}`);
